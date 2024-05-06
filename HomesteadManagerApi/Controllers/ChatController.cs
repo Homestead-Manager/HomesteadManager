cd3e2529b382f638d42ae8f690c7914515106b7a
darthjimmy <ejs138@gmail.com>
2024-05-06 16:42:58 -0400

    Add plant schema to GPT prompt

--- HomesteadManagerApi/Controllers/ChatController.cs
+++ HomesteadManagerApi/Controllers/ChatController.cs
@@ -8,6 +8,7 @@ namespace HomesteadManagerApi.Controllers
     public class ChatController : ControllerBase
     {
         private readonly IOpenAIService service;
+        private readonly string schema = "{\"Plant\": {\"CommonName\": \"\", \"ScientificName\": \"\", \"PlantType\": \"\", \"SunRequirement\": \"\", \"WaterRequirement\": \"\"}, \"ZoneID\": \"\", \"SowingMethod\": \"\", \"OptimalSowingPeriodStart\": \"yyyy-mm-dd\", \"OptimalSowingPeriodEnd\": \"yyyy-mm-dd\", \"HarvestPeriodStart\": \"yyyy-mm-dd\", \"HarvestPeriodEnd\": \"yyyy-mm-dd\", \"ZoneSpecificNotes\": \"\"}";
         public ChatController(IOpenAIService openAIService)
         {
             service = openAIService;
@@ -16,7 +17,7 @@ namespace HomesteadManagerApi.Controllers
         [HttpGet]
         public async Task<ActionResult<string>> GetPlants(string zipcode)
         {
-            return await service.CallEndpointAsync($"Plants that grow in {zipcode}");
+            return await service.CallEndpointAsync($"Plants that grow in {zipcode}. Respond with a list of JSON objects in this format: {schema}");
         }
     }
 }
--- HomesteadManagerApi/Models/OpenAi/Request.cs
+++ HomesteadManagerApi/Models/OpenAi/Request.cs
@@ -12,7 +12,7 @@ namespace HomesteadManagerApi.Models.OpenAi
 
         [JsonProperty("temperature")]
         public decimal Temperature { get; set; } = 0.7m;
-        public int max_tokens { get; set; } = 800;
+        public int max_tokens { get; set; } = 3800;
         public decimal top_p { get; set; } = 0.95m;
         public int frequency_penalty { get; set; } = 0;
         public int presence_penalty { get; set; } = 0;
