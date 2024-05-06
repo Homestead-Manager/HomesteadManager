23333067ea92bdcc638808b6c89ecf83447e5c05
Eddie Legg <eddie.legg@leadingedje.com>
2024-05-06 15:26:07 -0400

    set up homestead for wizard

--- HomesteadManagerApi/Models/Homestead.cs
+++ HomesteadManagerApi/Models/Homestead.cs
@@ -4,4 +4,11 @@ public class Homestead
 {
     public string? Id { get; set; }
     public string? Name { get; set; }
+    public string Zone { get; set; }
+    public string AddressLine1  { get; set; }
+    public string AddressLine2 { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string PostalCode { get; set; }
+
 }
