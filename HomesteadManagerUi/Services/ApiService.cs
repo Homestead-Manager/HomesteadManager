4386c695b6bfc4aecee4cce6ca7c06a4db139998
darthjimmy <ejs138@gmail.com>
2024-05-08 08:43:49 -0400

    Fix the Plant API call

--- HomesteadManagerUi/Services/ApiService.cs
+++ HomesteadManagerUi/Services/ApiService.cs
@@ -18,7 +18,7 @@ namespace HomesteadManagerUi.Services
 
         public async Task<List<PlantZoneInfo>> GetPlants(string zipCode)
         {
-            return await _httpClient.GetFromJsonAsync<List<PlantZoneInfo>>(zipCode);
+            return await _httpClient.GetFromJsonAsync<List<PlantZoneInfo>>($"/api/chat/plants?zipCode={zipCode}");
         }
     }
 }
